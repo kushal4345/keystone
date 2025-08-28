8fb53e01e79f38fd214348e0d27fbf772a8a1bad
Shash <ashishsinghlogic@gmail.com>
2025-08-29 00:54:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from '@/context/AppContext';
 import { HomePage } from '@/components/HomePage';
